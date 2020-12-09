21c0571c1aecd14238ef345ca470c47f0d8e790f
randyly8 <RaNdYlY8@gmail.com>
2020-12-09 17:57:10 -0600

    node updates on props

--- src/PathfindingVisualizer/Node.jsx
+++ src/PathfindingVisualizer/Node.jsx
@@ -1,18 +1,35 @@
 import React from 'react';
 
 function Node(props) {
-    const { isFinish, isStart } = props
+    const {
+        row,
+        col,
+        isFinish,
+        isStart,
+        isWall,
+        onMouseDown,
+        onMouseEnter,
+        onMouseUp
+    } = props
 
     // add className if condiction is true
     const extraClassName = isFinish
         ? 'node-finish'
         : isStart
             ? 'node-start'
-            : '';
+            : isWall
+                ? 'node-wall'
+                : '';
 
 
     return (
-        <div className={`node ${extraClassName}`}></div>
+        <div
+            id={`node-${row}-${col}`}
+            className={`node ${extraClassName}`}
+            onMouseDown={() => onMouseDown(row, col)}
+            onMouseEnter={() => onMouseEnter(row, col)}
+            onMouseUp={() => onMouseUp()}
+        ></div>
     );
 }
 
